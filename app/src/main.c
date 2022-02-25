c80466380d0893dd03a21ac2e373a6269aee0c46
DaaLuyckx <daan.luyckx@gmail.com>
2022-02-25 10:52:59 +0100

    a

--- app/src/main.c
+++ app/src/main.c
@@ -7,9 +7,28 @@
 #include <stdint.h>
 #include <stm32l4xx.h>
 
+void delay(unsigned int n) {
+	volatile unsigned int delay = n;
+	while (delay--)
+		;
+}
+
 int main(void) {
 
-  while (1) {
+
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
+
+
+	GPIOB->MODER &= ~GPIO_MODER_MODE9_Msk;
+	GPIOB->MODER |= GPIO_MODER_MODE9_0;
+	GPIOB->OTYPER &= ~GPIO_OTYPER_OT9;
+
+	while (1) {
+
+	  GPIOB->BSRR = GPIO_BSRR_BS9;
+	  delay(250);
+	  GPIOB->BSRR = GPIO_BSRR_BR9;
+	  delay(250);
 
   }
 }
