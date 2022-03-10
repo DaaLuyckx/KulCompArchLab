bb3c429453e882e12ad4033d8cc49c35dec45734
DaaLuyckx <daan.luyckx@gmail.com>
2022-03-10 19:51:06 +0100

    Update main.c

--- app/src/main.c
+++ app/src/main.c
@@ -7,28 +7,64 @@
 #include <stdint.h>
 #include <stm32l4xx.h>
 
-void delay(unsigned int n) {
+void delay(unsigned int n){
 	volatile unsigned int delay = n;
-	while (delay--)
-		;
+	while (delay--);
 }
 
 int main(void) {
 
-
+    //Klok activeren
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
+    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;
+
+    //Led1
+    GPIOB->MODER &= ~GPIO_MODER_MODE9_Msk; //Alle bits van pin9 (blok B) op 0 zetten
+    GPIOB->MODER |= GPIO_MODER_MODE9_0;    //Bit 0 van pin 9 hoog maken (=output)
+    GPIOB->OTYPER &= ~GPIO_OTYPER_OT9;     //Laag zetten voor push pull
+
+    //Led2
+    GPIOC->MODER &= ~GPIO_MODER_MODE13_Msk; //Alle bits van pin 13 (blok C) op 0 zetten
+    GPIOC->MODER |= GPIO_MODER_MODE13_0;    //Bit 0 van pin 13 hoog maken (=output)
+    GPIOC->OTYPER &= ~GPIO_OTYPER_OT13;     //Laag zetten voor push pull
+
+    //KnopA
+    GPIOB->MODER &= ~GPIO_MODER_MODE13_Msk; //Alle bits van pin 13 (blok B) laag zetten (=input)
+
+    //KnopB
+    GPIOB->MODER &= ~GPIO_MODER_MODE14_Msk; //Alle bits van pin 13 (blok B) laag zetten (=input)
+
+    //pull up weerstand hoog zetten
+    GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD13_Msk;
+    GPIOB->PUPDR |= GPIO_PUPDR_PUPD13_0;
+    GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD14_Msk;
+    GPIOB->PUPDR |= GPIO_PUPDR_PUPD14_0;
 
 
-	GPIOB->MODER &= ~GPIO_MODER_MODE9_Msk;
-	GPIOB->MODER |= GPIO_MODER_MODE9_0;
-	GPIOB->OTYPER &= ~GPIO_OTYPER_OT9;
+    while (1) {
+        if (!(GPIOB->IDR  & GPIO_IDR_ID13)) {
+            GPIOC->ODR |= GPIO_ODR_OD13;
+            GPIOB->ODR |= GPIO_ODR_OD9;
+            delay(500000);
+            GPIOC->ODR &= ~GPIO_ODR_OD13;
+            GPIOB->ODR &= ~GPIO_ODR_OD9;
+            delay(500000);
+        }
 
-	while (1) {
+        else if (!(GPIOB->IDR & GPIO_IDR_ID14)){
+            GPIOC->ODR |= GPIO_ODR_OD13;
+            GPIOB->ODR |= GPIO_ODR_OD9;
+            delay(2500000);
+            GPIOC->ODR &= ~GPIO_ODR_OD13;
+            GPIOB->ODR &= ~GPIO_ODR_OD9;
+            delay(2500000);
+            }
 
-	  GPIOB->BSRR = GPIO_BSRR_BS9;
-	  delay(250);
-	  GPIOB->BSRR = GPIO_BSRR_BR9;
-	  delay(250);
+        else {
+            GPIOC->ODR &= ~GPIO_ODR_OD13;
+            GPIOB->ODR &= ~GPIO_ODR_OD9;
+        }
+    }
 
-  }
+
 }
