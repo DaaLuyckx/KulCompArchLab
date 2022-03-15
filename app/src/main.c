76b9ac93d3737f818ea1fff6101eedc833d36b43
DaaLuyckx <daan.luyckx@gmail.com>
2022-03-15 19:05:58 +0100

    update

--- app/src/main.c
+++ app/src/main.c
@@ -12,8 +12,128 @@ void delay(unsigned int n){
 	while (delay--);
 }
 
+void segments(int n){
+//Hier zeg ik welke segmenten moeten aangaan bij welk cijfer.
+	switch (n){
+		case 0:
+			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD15 | GPIO_ODR_OD1);
+			break;
+
+		case 1:
+			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
+			break;
+
+		case 2:
+			GPIOA->ODR |= (GPIO_ODR_OD7);
+			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD15 | GPIO_ODR_OD2);
+			break;
+
+		case 3:
+			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD2);
+			break;
+
+		case 4:
+			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD1 | GPIO_ODR_OD2);
+			break;
+
+		case 5:
+			GPIOA->ODR |= (GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD1 | GPIO_ODR_OD2);
+			break;
+
+		case 6:
+			GPIOA->ODR |= (GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD15 | GPIO_ODR_OD1 | GPIO_ODR_OD2);
+			break;
+
+		case 7:
+			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD0);
+			break;
+
+		case 8:
+			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD15 | GPIO_ODR_OD1 | GPIO_ODR_OD2);
+			break;
+
+		case 9:
+			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD1 | GPIO_ODR_OD2);
+			break;
+	}
+
+}
+
+void clear_segments(){
+//Alle segmenten worden uitgezet.
+	GPIOA->ODR &= ~(GPIO_ODR_OD7 | GPIO_ODR_OD5);
+	GPIOB->ODR &= ~(GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD15 | GPIO_ODR_OD1 | GPIO_ODR_OD2);
+
+}
+
 int main(void) {
+	//klok
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
+
+	//segmenten
+	GPIOB->MODER &= ~GPIO_MODER_MODE0_Msk;
+	GPIOB->MODER |= GPIO_MODER_MODE0_0;
+	GPIOB->OTYPER &= ~GPIO_OTYPER_OT0;
+
+	GPIOA->MODER &= ~GPIO_MODER_MODE7_Msk;
+	GPIOA->MODER |= GPIO_MODER_MODE7_0;
+	GPIOA->OTYPER &= ~GPIO_OTYPER_OT7;
+
+	GPIOA->MODER &= ~GPIO_MODER_MODE5_Msk;
+	GPIOA->MODER |= GPIO_MODER_MODE5_0;
+	GPIOA->OTYPER &= ~GPIO_OTYPER_OT5;
+
+	GPIOB->MODER &= ~GPIO_MODER_MODE12_Msk;
+	GPIOB->MODER |= GPIO_MODER_MODE12_0;
+	GPIOB->OTYPER &= ~GPIO_OTYPER_OT12;
+
+	GPIOB->MODER &= ~GPIO_MODER_MODE15_Msk;
+	GPIOB->MODER |= GPIO_MODER_MODE15_0;
+	GPIOB->OTYPER &= ~GPIO_OTYPER_OT15;
+
+	GPIOB->MODER &= ~GPIO_MODER_MODE1_Msk;
+	GPIOB->MODER |= GPIO_MODER_MODE1_0;
+	GPIOB->OTYPER &= ~GPIO_OTYPER_OT1;
+
+	GPIOB->MODER &= ~GPIO_MODER_MODE2_Msk;
+	GPIOB->MODER |= GPIO_MODER_MODE2_0;
+	GPIOB->OTYPER &= ~GPIO_OTYPER_OT2;
+
+	//mux
+	GPIOA->MODER &= ~GPIO_MODER_MODE8_Msk;
+	GPIOA->MODER |= GPIO_MODER_MODE8_0;
+	GPIOA->OTYPER &= ~GPIO_OTYPER_OT8;
+
+	GPIOA->MODER &= ~GPIO_MODER_MODE15_Msk;
+	GPIOA->MODER |= GPIO_MODER_MODE15_0;
+	GPIOA->OTYPER &= ~GPIO_OTYPER_OT15;
+
+
+
 
   while (1) {
+
+	  GPIOA->ODR |= GPIO_ODR_OD8;
+	  GPIOA->ODR |= GPIO_ODR_OD15;
+
+	  for(int i = 0; i<=9; i++){
+		  segments(i);
+		  delay(2500000);
+		  clear_segments();
+		  delay(2500000);
+
+	  }
+
   }
+
 }
+
