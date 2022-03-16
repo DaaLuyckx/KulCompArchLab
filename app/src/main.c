0d8107cccf4b5520b42558fe20dea95186beb895
DaaLuyckx <daan.luyckx@gmail.com>
2022-03-16 17:17:31 +0100

    Update main.c

--- app/src/main.c
+++ app/src/main.c
@@ -7,11 +7,9 @@
 #include <stdint.h>
 #include <stm32l4xx.h>
 
-void EXTI15_10_IRQHandler(void){
-
-    if(EXTI->PR1 & EXTI_PR1_PIF14){
-        EXTI->PR1 = EXTI_PR1_PIF14;
-    }
+void delay(unsigned int n){
+	volatile unsigned int delay = n;
+	while (delay--);
 }
 
 int main(void) {
@@ -19,6 +17,16 @@ int main(void) {
 	// Klok voor GPIOB en SYSCFG aanzetten
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
 	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+
+	//Led1
+	GPIOB->MODER &= ~GPIO_MODER_MODE9_Msk; //Alle bits van pin9 (blok B) op 0 zetten
+	GPIOB->MODER |= GPIO_MODER_MODE9_0;    //Bit 0 van pin 9 hoog maken (=output)
+	GPIOB->OTYPER &= ~GPIO_OTYPER_OT9;     //Laag zetten voor push pull
+
+	//Led2
+	GPIOC->MODER &= ~GPIO_MODER_MODE13_Msk; //Alle bits van pin 13 (blok C) op 0 zetten
+	GPIOC->MODER |= GPIO_MODER_MODE13_0;    //Bit 0 van pin 13 hoog maken (=output)
+	GPIOC->OTYPER &= ~GPIO_OTYPER_OT13;     //Laag zetten voor push pull
 
 	// GPIOB Pin 14 configureren als input
 	GPIOB->MODER &= ~GPIO_MODER_MODE14_Msk;
@@ -41,5 +49,19 @@ int main(void) {
 
   while (1) {
   }
+}
+
+void EXTI15_10_IRQHandler(void){
+	GPIOC->ODR |= GPIO_ODR_OD13;
+    GPIOB->ODR |= GPIO_ODR_OD9;
+    delay(2500000);
+    GPIOC->ODR &= ~GPIO_ODR_OD13;
+    GPIOB->ODR &= ~GPIO_ODR_OD9;
+    delay(2500000);
+    if(EXTI->PR1 & EXTI_PR1_PIF14){
+        GPIOC->ODR |= GPIO_ODR_OD13;
+
+        EXTI->PR1 = EXTI_PR1_PIF14;
+    }
 }
 
