1d078c21d9b85aeabf6e7173cf176c551b304d97
DaaLuyckx <daan.luyckx@gmail.com>
2022-03-16 17:37:31 +0100

    Update main.c

--- app/src/main.c
+++ app/src/main.c
@@ -12,10 +12,34 @@ void delay(unsigned int n){
 	while (delay--);
 }
 
+void EXTI15_10_IRQHandler(void){
+
+    if(EXTI->PR1 & EXTI_PR1_PIF14){
+    	GPIOC->ODR |= GPIO_ODR_OD13;
+        GPIOB->ODR |= GPIO_ODR_OD9;
+        delay(2500000);
+        GPIOC->ODR &= ~GPIO_ODR_OD13;
+        GPIOB->ODR &= ~GPIO_ODR_OD9;
+        delay(2500000);
+        EXTI->PR1 = EXTI_PR1_PIF14;
+    }
+
+    if(EXTI->PR1 & EXTI_PR1_PIF13){
+    	GPIOC->ODR |= GPIO_ODR_OD13;
+        GPIOB->ODR |= GPIO_ODR_OD9;
+        delay(250000);
+        GPIOC->ODR &= ~GPIO_ODR_OD13;
+        GPIOB->ODR &= ~GPIO_ODR_OD9;
+        delay(250000);
+        EXTI->PR1 = EXTI_PR1_PIF13;
+    }
+}
+
 int main(void) {
 
 	// Klok voor GPIOB en SYSCFG aanzetten
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;
 	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
 
 	//Led1
@@ -28,22 +52,29 @@ int main(void) {
 	GPIOC->MODER |= GPIO_MODER_MODE13_0;    //Bit 0 van pin 13 hoog maken (=output)
 	GPIOC->OTYPER &= ~GPIO_OTYPER_OT13;     //Laag zetten voor push pull
 
-	// GPIOB Pin 14 configureren als input
+		//GPIOB Pin 14 configureren als input
 	GPIOB->MODER &= ~GPIO_MODER_MODE14_Msk;
-
-	// Pull up aanzetten op Pin 14 van GPIOB
+		//Pull up aanzetten op Pin 14 van GPIOB
 	GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD14_Msk;
 	GPIOB->PUPDR |= GPIO_PUPDR_PUPD14_0;
-
-	// Pin 14 van GPIOB routeren naar de EXTI
+		//Pin 14 van GPIOB routeren naar de EXTI
 	SYSCFG->EXTICR[3] &= ~SYSCFG_EXTICR4_EXTI14_Msk;
 	SYSCFG->EXTICR[3] |= SYSCFG_EXTICR4_EXTI14_PB;
-
-	// Falling edge interrupt aanzetten
+		//Falling edge interrupt aanzetten
 	EXTI->FTSR1 |= EXTI_FTSR1_FT14;
 	EXTI->IMR1 |= EXTI_IMR1_IM14;
-
-	// Interrupt aanzetten met een prioriteit van 128
+		//GPIOB Pin 13 configureren als input
+	GPIOB->MODER &= ~GPIO_MODER_MODE13_Msk;
+		//Pull up aanzetten op Pin 13 van GPIOB
+	GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD13_Msk;
+	GPIOB->PUPDR |= GPIO_PUPDR_PUPD13_0;
+		//Pin 13 van GPIOB routeren naar de EXTI
+	SYSCFG->EXTICR[3] &= ~SYSCFG_EXTICR4_EXTI13_Msk;
+	SYSCFG->EXTICR[3] |= SYSCFG_EXTICR4_EXTI13_PB;
+		//Falling edge interrupt aanzetten
+	EXTI->FTSR1 |= EXTI_FTSR1_FT13;
+	EXTI->IMR1 |= EXTI_IMR1_IM13;
+		//Interrupt aanzetten met een prioriteit van 128
 	NVIC_SetPriority(EXTI15_10_IRQn, 128);
 	NVIC_EnableIRQ(EXTI15_10_IRQn);
 
@@ -51,17 +82,5 @@ int main(void) {
   }
 }
 
-void EXTI15_10_IRQHandler(void){
-	GPIOC->ODR |= GPIO_ODR_OD13;
-    GPIOB->ODR |= GPIO_ODR_OD9;
-    delay(2500000);
-    GPIOC->ODR &= ~GPIO_ODR_OD13;
-    GPIOB->ODR &= ~GPIO_ODR_OD9;
-    delay(2500000);
-    if(EXTI->PR1 & EXTI_PR1_PIF14){
-        GPIOC->ODR |= GPIO_ODR_OD13;
 
-        EXTI->PR1 = EXTI_PR1_PIF14;
-    }
-}
 
