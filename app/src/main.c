303e137716985062c3a2cf5aa458460cedd6bd65
DaaLuyckx <daan.luyckx@gmail.com>
2022-03-18 12:17:43 +0100

    Update main.c

--- app/src/main.c
+++ app/src/main.c
@@ -9,10 +9,10 @@
 #include <stdint.h>
 #include <stm32l4xx.h>
 
-int number = 0;
+int number = 2031;
 int tick = 0;
-int toggle_A = 0;
-int toggle_B = 0;
+int splitted_number[4] = {0};
+int toggle =0;
 
 void delay(unsigned int n){  //deze sebiet is testen.
 	volatile unsigned int delay = n;
@@ -79,64 +79,73 @@ void clear_segments(){
 	GPIOB->ODR &= ~(GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD15 | GPIO_ODR_OD1 | GPIO_ODR_OD2);
 }
 
-void number_to_segments(int n){
-// Zet het getal om in de segmenten per module
-	int thousand = (n/1000)%10;
-	int hundred = (n/100)%10;
-	int ten = (n/10)%10;
-	int unit = n%10;
-
-//Dit stukje is omdat het een klok is (max waarden van de 'minuten, en 'uren' aanpassen)
-	if (ten == 6 && unit == 0){
-		ten = 0;
-		hundred++;
-		number = (thousand*1000+hundred*100+ten*10+unit);
-	}
-	if (thousand == 2 && hundred == 4){
-		thousand = 0;
-		hundred = 0;
-		number = (thousand*1000+hundred*100+ten*10+unit);
+void split_number(int n){
+	// Zet het getal om in de segmenten per module
+		int thousand = (n/1000)%10;
+		int hundred = (n/100)%10;
+		int ten = (n/10)%10;
+		int unit = n%10;
+		splitted_number[0] = thousand;
+		splitted_number[1] = hundred;
+		splitted_number[2] = ten;
+		splitted_number[3] = unit;
 }
 
-//Stuur de waardes naar
-	//00
-	GPIOA-> ODR &= ~GPIO_ODR_OD8;
-	GPIOA-> ODR &= ~GPIO_ODR_OD15;
-	segments(thousand);
-	clear_segments();
 
-	//01
-	GPIOA-> ODR |= GPIO_ODR_OD8;
-	GPIOA-> ODR &= ~GPIO_ODR_OD15;
-	segments(hundred);
-	clear_segments();
+void check_clock(int* n){
+	//Dit stukje is omdat het een klok is (max waarden van de 'minuten, en 'uren' aanpassen)
+		if (splitted_number[2] == 6 && splitted_number[3] == 0){
+			splitted_number[2] = 0;
+			splitted_number[1]++;
 
-	//10
-	GPIOA-> ODR &= ~GPIO_ODR_OD8;
-	GPIOA-> ODR |= GPIO_ODR_OD15;
-	segments(ten);
-	clear_segments();
-
-	//11
-	GPIOA-> ODR |= GPIO_ODR_OD8;
-	GPIOA-> ODR |= GPIO_ODR_OD15;
-	segments(unit);
-	clear_segments();
+		}
+		if (splitted_number[0] == 2 && splitted_number[1] == 4){
+			splitted_number[0] = 0;
+			splitted_number[1] = 0;
+		}
 }
 
 void SysTick_Handler(void){
     tick++;
-}
-
-void EXTI15_10_IRQHandler(void){
-    if(EXTI->PR1 & EXTI_PR1_PIF14){
-    	toggle_A++;
-        EXTI->PR1 = EXTI_PR1_PIF14; //Zet de flag terug laag, anders gaat hij terug in die direct interrupt na dat die gedaan is.
+    split_number(number);
+    check_clock(splitted_number);
+    switch(toggle){
+    case 0:
+    	//00
+    	clear_segments();
+    	GPIOA-> ODR &= ~GPIO_ODR_OD8;
+    	GPIOA-> ODR &= ~GPIO_ODR_OD15;
+    	segments(splitted_number[0]);
+    	toggle++;
+    	break;
+    case 1:
+    	//01
+    	clear_segments();
+    	GPIOA-> ODR |= GPIO_ODR_OD8;
+    	GPIOA-> ODR &= ~GPIO_ODR_OD15;
+    	segments(splitted_number[1]);
+    	toggle++;
+    	break;
+    case 2:
+    	//10
+    	clear_segments();
+    	GPIOA-> ODR &= ~GPIO_ODR_OD8;
+    	GPIOA-> ODR |= GPIO_ODR_OD15;
+    	segments(splitted_number[2]);
+    	toggle++;
+    	break;
+    case 3:
+    	//11
+    	clear_segments();
+    	GPIOA-> ODR |= GPIO_ODR_OD8;
+    	GPIOA-> ODR |= GPIO_ODR_OD15;
+    	segments(splitted_number[3]);
+    	toggle = 0;
     }
-
-    if(EXTI->PR1 & EXTI_PR1_PIF13){
-    	toggle_B++;
-        EXTI->PR1 = EXTI_PR1_PIF13;
+	number = (splitted_number[0]*1000+splitted_number[1]*100+splitted_number[2]*10+splitted_number[3]);
+    if(tick == 60000){
+    	number++;
+    	tick = 0;
     }
 }
 
@@ -209,69 +218,21 @@ int main(void){
     NVIC_SetPriority(SysTick_IRQn, 128);
     NVIC_EnableIRQ(SysTick_IRQn);
 
-    //interrupts buttons
-		//GPIOB Pin 14 configureren als input
-	GPIOB->MODER &= ~GPIO_MODER_MODE14_Msk;
-		//Pull up aanzetten op Pin 14 van GPIOB
-	GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD14_Msk;
-	GPIOB->PUPDR |= GPIO_PUPDR_PUPD14_0;
-		//Pin 14 van GPIOB routeren naar de EXTI
-	SYSCFG->EXTICR[3] &= ~SYSCFG_EXTICR4_EXTI14_Msk;
-	SYSCFG->EXTICR[3] |= SYSCFG_EXTICR4_EXTI14_PB;
-		//Falling edge interrupt aanzetten
-	EXTI->FTSR1 |= EXTI_FTSR1_FT14;
-	EXTI->IMR1 |= EXTI_IMR1_IM14;
-
-		//GPIOB Pin 13 configureren als input
-	GPIOB->MODER &= ~GPIO_MODER_MODE13_Msk;
-		//Pull up aanzetten op Pin 13 van GPIOB
-	GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD13_Msk;
-	GPIOB->PUPDR |= GPIO_PUPDR_PUPD13_0;
-		//Pin 13 van GPIOB routeren naar de EXTI
-	SYSCFG->EXTICR[3] &= ~SYSCFG_EXTICR4_EXTI13_Msk;
-	SYSCFG->EXTICR[3] |= SYSCFG_EXTICR4_EXTI13_PB;
-		//Falling edge interrupt aanzetten
-	EXTI->FTSR1 |= EXTI_FTSR1_FT13;
-	EXTI->IMR1 |= EXTI_IMR1_IM13;
-		//Interrupt aanzetten met een prioriteit van 127
-	NVIC_SetPriority(EXTI15_10_IRQn, 127);
-	NVIC_EnableIRQ(EXTI15_10_IRQn);
-
     while(1){
-    	switch (toggle_A){
-    		case 0:
-				number_to_segments(number);
-				if(tick == 1000){
-					number++;
-					tick = 0;
-				}
-				break;
-
-    		case 1:
-    			GPIOC->ODR |= GPIO_ODR_OD13;
-    			number_to_segments(number);
-    			if (toggle_B){
-    				number += 100;
-    				toggle_B = 0;
-    			}
-				break;
-
-    		case 2:
-    			GPIOC->ODR &= ~GPIO_ODR_OD13;
-    			GPIOB->ODR |= GPIO_ODR_OD9;
-    			number_to_segments(number);
-    			if (toggle_B){
-    				number ++;
-    				toggle_B = 0;
-    			}
-    			break;
-
-    		case 3:
-    			GPIOB->ODR &= ~GPIO_ODR_OD9;
-    			toggle_A = 0;
-    			tick = 0;
-    			delay(100000);
-    			break;
+    	if (!(GPIOB->IDR & GPIO_IDR_ID14)){
+    		GPIOC->ODR |= GPIO_ODR_OD13;
+    		number +=100;
+    		delay(1000000);
     	}
+
+    	if(!(GPIOB->IDR & GPIO_IDR_ID13)){
+    		GPIOB->ODR |= GPIO_ODR_OD9;
+    		number +=1;
+    		delay(1000000);
+
+    	}
+    	delay(1000);
+    	GPIOC->ODR &= ~GPIO_ODR_OD13;
+    	GPIOB->ODR &= ~GPIO_ODR_OD9;
     }
 }
