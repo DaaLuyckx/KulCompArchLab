5f6a2b8a96cce45b180500bda0674c729676bfe7
DaaLuyckx <daan.luyckx@gmail.com>
2022-05-12 17:30:56 +0200

    Update main.c

--- app/src/main.c
+++ app/src/main.c
@@ -6,136 +6,29 @@
 #include <stdint.h>
 #include <stm32l4xx.h>
 #include <math.h>
+#include <stdio.h>
 
 int tick = 0;
 int splitted_number[4] = {0};
 int toggle =0;
 float number;
 
-void delay(unsigned int n){
+/*void delay(unsigned int n){
 	volatile unsigned int delay = n;
-	while (delay != 0){
-		if (tick){
+	while (delay !=0){
+		if(tick){
 			delay--;
 			tick = 0;
 		}
 	}
+}*/
+
+void delay(unsigned int n){
+	volatile unsigned int delay = n;
+	while (delay--);
 }
 
-void segments(int n){
-//Hier zeg ik welke segmenten moeten aangaan bij welk cijfer.
-	switch (n){
-		case 0:
-			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
-			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD15 | GPIO_ODR_OD1);
-			break;
-
-		case 1:
-			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
-			break;
-
-		case 2:
-			GPIOA->ODR |= (GPIO_ODR_OD7);
-			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD15 | GPIO_ODR_OD2);
-			break;
-
-		case 3:
-			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
-			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD2);
-			break;
-
-		case 4:
-			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
-			GPIOB->ODR |= (GPIO_ODR_OD1 | GPIO_ODR_OD2);
-			break;
-
-		case 5:
-			GPIOA->ODR |= (GPIO_ODR_OD5);
-			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD1 | GPIO_ODR_OD2);
-			break;
-
-		case 6:
-			GPIOA->ODR |= (GPIO_ODR_OD5);
-			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD15 | GPIO_ODR_OD1 | GPIO_ODR_OD2);
-			break;
-
-		case 7:
-			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
-			GPIOB->ODR |= (GPIO_ODR_OD0);
-			break;
-
-		case 8:
-			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
-			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD15 | GPIO_ODR_OD1 | GPIO_ODR_OD2);
-			break;
-
-		case 9:
-			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
-			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD1 | GPIO_ODR_OD2);
-			break;
-	}
-}
-
-void clear_segments(){
-//Alle segmenten worden uitgezet.
-	GPIOA->ODR &= ~(GPIO_ODR_OD7 | GPIO_ODR_OD5);
-	GPIOB->ODR &= ~(GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD15 | GPIO_ODR_OD1 | GPIO_ODR_OD2);
-}
-
-void split_number(int n){
-	// Zet het getal om in de segmenten per module
-		int thousand = (n/1000)%10;
-		int hundred = (n/100)%10;
-		int ten = (n/10)%10;
-		int unit = n%10;
-		splitted_number[0] = thousand;
-		splitted_number[1] = hundred;
-		splitted_number[2] = ten;
-		splitted_number[3] = unit;
-}
-
-void SysTick_Handler(void){
-    tick++;
-    split_number(number);
-    switch(toggle){
-    case 0:
-    	//00
-    	clear_segments();
-    	GPIOA-> ODR &= ~GPIO_ODR_OD8;
-    	GPIOA-> ODR &= ~GPIO_ODR_OD15;
-    	GPIOA-> ODR &= ~GPIO_ODR_OD6;
-    	segments(splitted_number[0]);
-    	toggle++;
-    	break;
-    case 1:
-    	//01
-    	clear_segments();
-    	GPIOA-> ODR |= GPIO_ODR_OD8;
-    	GPIOA-> ODR &= ~GPIO_ODR_OD15;
-    	GPIOA-> ODR &= ~GPIO_ODR_OD6;
-    	segments(splitted_number[1]);
-    	toggle++;
-    	break;
-    case 2:
-    	//10
-    	clear_segments();
-    	GPIOA-> ODR &= ~GPIO_ODR_OD8;
-    	GPIOA-> ODR |= GPIO_ODR_OD15;
-    	GPIOA-> ODR |= GPIO_ODR_OD6; //puntje
-    	segments(splitted_number[2]);
-    	toggle++;
-    	break;
-    case 3:
-    	//11
-    	clear_segments();
-    	GPIOA-> ODR |= GPIO_ODR_OD8;
-    	GPIOA-> ODR |= GPIO_ODR_OD15;
-    	GPIOA-> ODR &= ~GPIO_ODR_OD6;
-    	segments(splitted_number[3]);
-    	toggle = 0;
-    }
-	number = (splitted_number[0]*1000+splitted_number[1]*100+splitted_number[2]*10+splitted_number[3]);
-}
+void SysTick_Handler(void){tick++;}
 
 int readNTC(){
 	ADC1->SQR1 |= (ADC_SQR1_SQ1_0 | ADC_SQR1_SQ1_2); //00101 (5 binair)
@@ -145,7 +38,6 @@ int readNTC(){
 	while(!(ADC1->ISR & ADC_ISR_EOS));
 
 	return ADC1->DR;
-
 }
 
 int __io_putchar(int ch){
@@ -154,123 +46,65 @@ int __io_putchar(int ch){
 }
 
 int main(void){
-	//gebruik uart
+
+	//klok
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
+	RCC->AHB2ENR |= RCC_AHB2ENR_ADCEN;
 	RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
 
+	//Configuratie ADC
+	// Klok selecteren, hier gebruiken we sysclk
+	RCC->CCIPR &= ~RCC_CCIPR_ADCSEL_Msk;
+	RCC->CCIPR |= RCC_CCIPR_ADCSEL_0 | RCC_CCIPR_ADCSEL_1;
+	//NTC
+	GPIOA->MODER &= ~GPIO_MODER_MODE0_Msk;
+	GPIOA->MODER |= GPIO_MODER_MODE0_0 | GPIO_MODER_MODE0_1;
+	// Deep powerdown modus uitzetten
+	ADC1->CR &= ~ADC_CR_DEEPPWD;
+	// ADC voltage regulator aanzetten
+	ADC1->CR |= ADC_CR_ADVREGEN;
+	//Wachten tot de regulator is gestabiliseerd (max. 20 us)
+	delay(2000);
+	// Kalibratie starten
+	ADC1->CR |= ADC_CR_ADCAL;
+	while(ADC1->CR & ADC_CR_ADCAL);
+	// ADC aanzetten
+	ADC1->CR |= ADC_CR_ADEN;
+	// Kanaal (6) instellen
+	ADC1->SMPR1 |= ADC_SMPR1_SMP5_0 | ADC_SMPR1_SMP5_1 | ADC_SMPR1_SMP5_2; //(111, traagste samplefrequentie.. beste)
+	ADC1->SQR1 &= ~(ADC_SQR1_L_0 | ADC_SQR1_L_1 | ADC_SQR1_L_2 | ADC_SQR1_L_3); //(lengte' aantal in te lezen kanalen (1= 000)
+
+	//Configuratie UART
+	//GPIO configureren en alternative mode aanzetten
 	GPIOA->MODER &= ~GPIO_MODER_MODE9_Msk;
 	GPIOA->MODER |=  GPIO_MODER_MODE9_1;
 	GPIOA->OTYPER &= ~GPIO_OTYPER_OT9;
 	GPIOA->AFR[1] = (GPIOA->AFR[1] & (~GPIO_AFRH_AFSEL9_Msk)) | (0x7 << GPIO_AFRH_AFSEL9_Pos);
-
 	GPIOA->MODER &= ~GPIO_MODER_MODE10_Msk;
 	GPIOA->AFR[1] = (GPIOA->AFR[1] & (~GPIO_AFRH_AFSEL10_Msk)) | (0x7 << GPIO_AFRH_AFSEL10_Pos);
-
+	//UART configureren
 	USART1->CR1 = 0;
 	USART1->CR2 = 0;
 	USART1->CR3 = 0;
 	USART1->BRR = 417;
 	USART1->CR1 = USART_CR1_TE | USART_CR1_RE | USART_CR1_UE;
 
-	//klok
-	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
-	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
-	RCC->AHB2ENR |= RCC_AHB2ENR_ADCEN;
-
-	// Klok selecteren, hier gebruiken we sysclk
-	RCC->CCIPR &= ~RCC_CCIPR_ADCSEL_Msk;
-	RCC->CCIPR |= RCC_CCIPR_ADCSEL_0 | RCC_CCIPR_ADCSEL_1;
-
-	//NTC
-	GPIOA->MODER &= ~GPIO_MODER_MODE0_Msk;
-	GPIOA->MODER |= GPIO_MODER_MODE0_0 | GPIO_MODER_MODE0_1;
-
-	// Deep powerdown modus uitzetten
-	ADC1->CR &= ~ADC_CR_DEEPPWD;
-
-	// ADC voltage regulator aanzetten
-	ADC1->CR |= ADC_CR_ADVREGEN;
-
-	//Wachten tot de regulator is gestabiliseerd (max. 20 us)
-	delay(4);
-
-	// Kalibratie starten
-	ADC1->CR |= ADC_CR_ADCAL;
-	while(ADC1->CR & ADC_CR_ADCAL);
-
-	// ADC aanzetten
-	ADC1->CR |= ADC_CR_ADEN;
-
-	// Kanaal (6) instellen
-	ADC1->SMPR1 |= ADC_SMPR1_SMP5_0 | ADC_SMPR1_SMP5_1 | ADC_SMPR1_SMP5_2; //(111, traagste samplefrequentie.. beste)
-	ADC1->SQR1 &= ~(ADC_SQR1_L_0 | ADC_SQR1_L_1 | ADC_SQR1_L_2 | ADC_SQR1_L_3); //(lengte' aantal in te lezen kanalen (1= 000)
-
-
-	//segmenten
-	GPIOB->MODER &= ~GPIO_MODER_MODE0_Msk;
-	GPIOB->MODER |= GPIO_MODER_MODE0_0;
-	GPIOB->OTYPER &= ~GPIO_OTYPER_OT0;
-
-	GPIOA->MODER &= ~GPIO_MODER_MODE7_Msk;
-	GPIOA->MODER |= GPIO_MODER_MODE7_0;
-	GPIOA->OTYPER &= ~GPIO_OTYPER_OT7;
-
-	GPIOA->MODER &= ~GPIO_MODER_MODE5_Msk;
-	GPIOA->MODER |= GPIO_MODER_MODE5_0;
-	GPIOA->OTYPER &= ~GPIO_OTYPER_OT5;
-
-	GPIOB->MODER &= ~GPIO_MODER_MODE12_Msk;
-	GPIOB->MODER |= GPIO_MODER_MODE12_0;
-	GPIOB->OTYPER &= ~GPIO_OTYPER_OT12;
-
-	GPIOB->MODER &= ~GPIO_MODER_MODE15_Msk;
-	GPIOB->MODER |= GPIO_MODER_MODE15_0;
-	GPIOB->OTYPER &= ~GPIO_OTYPER_OT15;
-
-	GPIOB->MODER &= ~GPIO_MODER_MODE1_Msk;
-	GPIOB->MODER |= GPIO_MODER_MODE1_0;
-	GPIOB->OTYPER &= ~GPIO_OTYPER_OT1;
-
-	GPIOB->MODER &= ~GPIO_MODER_MODE2_Msk;
-	GPIOB->MODER |= GPIO_MODER_MODE2_0;
-	GPIOB->OTYPER &= ~GPIO_OTYPER_OT2;
-	//puntje
-	GPIOA->MODER &= ~GPIO_MODER_MODE6_Msk;
-	GPIOA->MODER |= GPIO_MODER_MODE6_0;
-	GPIOA->OTYPER &= ~GPIO_OTYPER_OT6;
-
-	//multiplexer
-	GPIOA->MODER &= ~GPIO_MODER_MODE8_Msk;
-	GPIOA->MODER |= GPIO_MODER_MODE8_0;
-	GPIOA->OTYPER &= ~GPIO_OTYPER_OT8;
-
-	GPIOA->MODER &= ~GPIO_MODER_MODE15_Msk;
-	GPIOA->MODER |= GPIO_MODER_MODE15_0;
-	GPIOA->OTYPER &= ~GPIO_OTYPER_OT15;
-
-	//buttons
-    GPIOB->MODER &= ~GPIO_MODER_MODE13_Msk;
-    GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD13_Msk;
-    GPIOB->PUPDR |= GPIO_PUPDR_PUPD13_0;
-
-    GPIOB->MODER &= ~GPIO_MODER_MODE14_Msk;
-    GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD14_Msk;
-    GPIOB->PUPDR |= GPIO_PUPDR_PUPD14_0;
-
-    //systick configureren + interupt aanzetten + prioriteit geven
+    //Systick configureren + interupt aanzetten + prioriteit geven
     SysTick_Config(48000);
     NVIC_SetPriority(SysTick_IRQn, 128);
     NVIC_EnableIRQ(SysTick_IRQn);
 
     while(1){
     	// Lees de waarde in en maak conversie.
-    	float value = readNTC();
-    	float V = (value*3.0f)/4096.0f;
+    	float Raw = readNTC();
+    	float V = (Raw*3.0f)/4096.0f;
     	float R = (10000.0f*V)/(3.0f-V);
     	number = (1.0f/((logf(R/10000.0f)/3936.0f)+(1.0f/298.15f)))-273.15f;
 
-    	printf("T=%.1fºC\n\r ",number); // @suppress("Float formatting support")
-    	delay(1000);
+    	printf("T=%.1fºC\n",number); // @suppress("Float formatting support")
+
+    	delay(10000000); //1 meting per seconde (ongeveer)
     }
 
 }
